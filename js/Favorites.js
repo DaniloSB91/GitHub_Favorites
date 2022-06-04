b3f13c5c8a70f16bf12a2cbf4327b1f12b4369f4
DaniloSB91 <danilosilva.barao@gmail.com>
2022-06-04 16:11:59 -0300

    Projeto finalizado

--- js/Favorites.js
+++ js/Favorites.js
@@ -1,3 +1,5 @@
+import { GithubUsers } from "./GithubUser.js"
+
 //class que vai conter a lógica dos dados
 // como os dados são estruturados 
 
@@ -5,24 +7,67 @@ export class Favorites {
     constructor(root) {
         this.root = document.querySelector(root)
         this.load()
+
+
+
+
     }
 
-    load(){
-        this.entries =  [
-            {
-                login: 'DaniloSB91',
-                name: "Danilo SB",
-                public_repos: '76',
-                followers: '12.000'
-            },
-            {
-                login: 'Jose',
-                name: "JJ s",
-                public_repos: '768',
-                followers: '12.120'
+
+
+    load() {
+
+        this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
+    }
+
+    save(){
+        localStorage.setItem('@github-favorites:', JSON.stringify(this.entries))
+    }
+
+    async add(username) {
+
+        try {
+
+            const userExists = this.entries.find(entry => entry.login === username)
+
+            console.log(userExists)
+            if(userExists){
+                throw new Error('Usuário já cadastrado')
             }
-        ]
-    
+
+
+
+            const user = await GithubUsers.search(username)
+         
+
+            if (user.login === undefined) {
+                throw new Error('Usuário não encontrado!')
+            }
+
+            this.entries = [user, ...this.entries]
+            this.update()
+            this.save()
+
+        } catch (error) {
+            alert(error.message)
+
+        }
+
+
+    }
+
+    delete(user) {
+        // Higher-order functions (map, filter, find, reduce )
+        const filteredEntries = this.entries
+            .filter(entry => entry.login !== user.login)
+
+        this.entries = filteredEntries
+
+
+        console.log(filteredEntries)
+
+        this.update()
+        this.save()
     }
 }
 
@@ -35,26 +80,45 @@ export class FavoritesView extends Favorites {
         super(root)
 
         this.tbody = this.root.querySelector('table tbody')
-        
 
+        this.onadd()
         this.update()
+    }
+
+    onadd() {
+        const addButton = this.root.querySelector('.search button')
+        addButton.onclick = () => {
+            const { value } = this.root.querySelector('.search input')
+            this.add(value)
+        }
+
     }
 
     update() {
         this.removeAllTer()
 
         this.entries.forEach(user => {
-         const row = this.createRow()
-         
-         row.querySelector('.user img').src = `http://GitHub.com/${user.login}.png` 
-         row.querySelector('.user img').alt = `Imagem de ${user.name}`
-         row.querySelector('.user p').textContent = user.name 
-         row.querySelector('.user spam').textContent = user.login
-         row.querySelector('.repositories').textContent = user.public_repos
-         row.querySelector('.followers').textContent = user.followers  
+            const row = this.createRow()
 
+            row.querySelector('.user img').src = `http://GitHub.com/${user.login}.png`
+            row.querySelector('.user a').href = `http://GitHub.com/${user.login}`
 
-        this.tbody.append(row)
+            row.querySelector('.user img').alt = `Imagem de ${user.name}`
+            row.querySelector('.user p').textContent = user.name
+            row.querySelector('.user spam').textContent = user.login
+            row.querySelector('.repositories').textContent = user.public_repos
+            row.querySelector('.followers').textContent = user.followers
+
+            row.querySelector('.remove').onclick = () => {
+                const isOK = confirm('Tem certeza que deseja deletar essa linha?')
+
+                if (isOK) {
+                    this.delete(user)
+
+                }
+            }
+
+            this.tbody.append(row)
 
         })
     }
